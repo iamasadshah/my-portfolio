267e5425f2ff0874e1c9305864298752d644c690
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-10-07 11:28:42 +0500

    services page added

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -1,7 +1,48 @@
+import { Card } from "@/components/ui/card";
 import React from "react";
-
+import Image from "next/image";
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="mt-4">
+      <h1 className="text-3xl font-semibold text-center">
+        Take a look at the variety of services I offer.
+      </h1>
+      <div className="grid grid-cols-1 px-6 gap-4 mt-4 md:grid-cols-2 ">
+        <Card className="services-card">
+          <Image alt="image" src="/ux-design.png" width={100} height={100} />
+          <h3 className="service-heading">UI/UX Design</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio sequi
+            non ipsam unde laborum aliquid sint quam officia ipsum saepe?
+          </p>
+        </Card>
+        <Card className="services-card">
+          <Image alt="image" src="/development.png" width={100} height={100} />
+          <h3 className="service-heading">Frontend Development</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio sequi
+            non ipsam unde laborum aliquid sint quam officia ipsum saepe?
+          </p>
+        </Card>
+        <Card className="services-card">
+          <Image alt="image" src="/SPA.png" width={100} height={100} />
+          <h3 className="service-heading">Single Page Applications (SPAs)</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio sequi
+            non ipsam unde laborum aliquid sint quam officia ipsum saepe?
+          </p>
+        </Card>
+        <Card className="services-card">
+          <Image alt="image" src="/seo.png" width={100} height={100} />
+          <h3 className="service-heading">SEO Optimization</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio sequi
+            non ipsam unde laborum aliquid sint quam officia ipsum saepe?
+          </p>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
