e09eb25475b6df138fc748e9553ead63bdfc7ca0
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-10-13 00:59:39 +0500

    Working on FAQs section

--- src/app/components/FAQs.tsx
+++ src/app/components/FAQs.tsx
@@ -10,7 +10,7 @@ import React from "react";
 const FAQs = () => {
   return (
     <div
-      className="flex flex-col items-center justify-center h-screen bg-cover bg-center bg-no-repeat px-6"
+      className="flex flex-col items-center justify-center h-screen bg-cover bg-center bg-no-repeat px-6 py-10"
       style={{
         backgroundImage: "url('/second-bg.svg')", // Replace with your image path
       }}
@@ -23,27 +23,33 @@ const FAQs = () => {
         Services.
       </p>
 
-      <Accordion type="multiple" className="w-full max-w-md">
+      <Accordion type="single" className="w-full max-w-md">
         <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionTrigger className="text-md md:text-lg lg:text-xl">
+            Is it accessible?
+          </AccordionTrigger>
           <AccordionContent>
             Yes. It adheres to the WAI-ARIA design pattern.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionTrigger className="text-md md:text-lg lg:text-xl">
+            Is it accessible?
+          </AccordionTrigger>
           <AccordionContent>
             Yes. It adheres to the WAI-ARIA design pattern.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-3">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionTrigger className="text-md md:text-lg lg:text-xl">
+            Is it accessible?
+          </AccordionTrigger>
           <AccordionContent>
             Yes. It adheres to the WAI-ARIA design pattern.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-4">
-          <AccordionTrigger className="text-md">
+          <AccordionTrigger className="text-md md:text-lg lg:text-xl">
             Is it accessible?
           </AccordionTrigger>
           <AccordionContent>
@@ -51,18 +57,26 @@ const FAQs = () => {
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-5">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-6">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionTrigger className="text-md md:text-lg lg:text-xl">
+            Is it accessible?
+          </AccordionTrigger>
           <AccordionContent>
             Yes. It adheres to the WAI-ARIA design pattern.
           </AccordionContent>
         </AccordionItem>
       </Accordion>
+
+      <div className="flex flex-col mb-12 mt-3 rounded-lg bg-primary2 justify-center items-center px-16 py-6">
+        <h1 className="text-2xl font-bold mb-4 text-center">
+          Still have Question?
+        </h1>
+        <p className="text-lg mb-8 text-center">
+          Contact us for more information.
+        </p>
+        <button className="bg-primary1 transition-all duration-300 font-semibold hover:bg-gray-300 hover:text-primary1 hover:ring-2 hover:ring-primary1 hover:scale-105 text-white px-4 py-2 rounded-md">
+          Contact Us
+        </button>
+      </div>
     </div>
   );
 };
