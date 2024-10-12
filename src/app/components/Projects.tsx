76fc1a6ace461bd8f383de60b9a9c382c58956e4
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-10-12 14:47:19 +0500

    project images added

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,9 +1,14 @@
 import { Card } from "@/components/ui/card";
 import React from "react";
-
+import Image from "next/image";
+import { Tags } from "lucide-react";
 const Projects = () => {
   return (
-    <div>
+    <div
+      style={{
+        backgroundImage: "url('/first-bg.svg')", // Replace with your image path
+      }}
+    >
       <p className="text-center text-sm">Projects</p>
       <h1 className="text-center text-4xl font-bold">
         Discover My Success Stories
@@ -11,11 +16,43 @@ const Projects = () => {
       <p className="text-center text-sm">
         Explore My curated collection of successful projects.
       </p>
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <Card>Project one</Card>
-        <Card>Project two</Card>
-        <Card>Project three</Card>
-        <Card>Project four</Card>
+      <div className="grid grid-cols-1 md:grid-cols-2 px-4 gap-y-3">
+        <Card className="project-card">
+          <Image
+            src={"/project-image.jpg"}
+            alt="project one"
+            width={300}
+            height={300}
+          />
+          <h1>AirFlow X SaaS Website Template</h1>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque, quam.
+          Eaque earum quo consectetur corrupti.
+          <Tags>project 1</Tags>
+        </Card>
+        <Card className="project-card">
+          <Image
+            src={"/project-image.jpg"}
+            alt="project one"
+            width={300}
+            height={300}
+          />
+        </Card>
+        <Card className="project-card">
+          <Image
+            src={"/project-image.jpg"}
+            alt="project one"
+            width={300}
+            height={300}
+          />
+        </Card>
+        <Card className="project-card">
+          <Image
+            src={"/project-image.jpg"}
+            alt="project one"
+            width={300}
+            height={300}
+          />
+        </Card>
       </div>
     </div>
   );
